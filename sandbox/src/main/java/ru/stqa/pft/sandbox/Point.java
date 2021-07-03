1c44de29663bb5452cc53d0ea14607df6df95cb5
staylazyy <tresh45rus@gmail.com>
2021-07-03 15:12:10 +0300

    created method Point
    homework

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+  public double p1;
+  public double p2;
+  public double p3;
+  public double p4;
+
+  public Point(double p1, double p2, double p3, double p4) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
+    this.p4 = p4;
+  }
+
+  public double distance() {
+    return Math.sqrt(Math.pow((this.p3 - this.p1),2) + Math.pow((this.p4 - this.p2), 2));
+  }
+}
