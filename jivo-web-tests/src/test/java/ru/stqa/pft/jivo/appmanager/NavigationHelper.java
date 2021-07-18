6b0b35601e4bc3697f3d458b4c48b32c1ccc9002
staylazyy <tresh45rus@gmail.com>
2021-07-18 11:31:52 +0300

    fixed driver is null error

--- jivo-web-tests/src/test/java/ru/stqa/pft/jivo/appmanager/NavigationHelper.java
+++ jivo-web-tests/src/test/java/ru/stqa/pft/jivo/appmanager/NavigationHelper.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
-  private WebDriver driver;
 
   public NavigationHelper(WebDriver driver) {
     super(driver);
